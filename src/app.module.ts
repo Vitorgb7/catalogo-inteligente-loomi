5d270327601d843a4da05468262078dcaa4a4a28
vitor <vitorgabriel181004@gmail.com>
2025-04-27 18:52:16 -0300

    feat: implementando o module do chatbot e integrando ele no module central da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TintaModule } from './module/tinta/tinta.module';
+import { ChatbotModule } from './module/chatbot/chatbot.module';
+
 
 @Module({
-  imports: [TintaModule],
+  imports: [TintaModule, ChatbotModule],
   controllers: [AppController],
   providers: [AppService],
 })
